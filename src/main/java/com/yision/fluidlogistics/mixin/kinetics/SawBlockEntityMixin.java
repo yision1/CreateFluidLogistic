0a4ceaf79f4543a0dffe0e8712f73e781a3ab127
yision <2144287136@qq.com>
2026-05-21 13:49:26 +0800

    修复动力锯拆包流体包裹出压缩储罐

---
+++ src/main/java/com/yision/fluidlogistics/mixin/kinetics/SawBlockEntityMixin.java
@@ -0,0 +1,28 @@
+package com.yision.fluidlogistics.mixin.kinetics;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.simibubi.create.content.kinetics.saw.SawBlockEntity;
+import com.simibubi.create.content.processing.recipe.ProcessingInventory;
+import com.yision.fluidlogistics.item.FluidPackageItem;
+
+@Mixin(SawBlockEntity.class)
+public abstract class SawBlockEntityMixin {
+
+    @Shadow
+    public ProcessingInventory inventory;
+
+    @Inject(method = "applyRecipe", at = @At("HEAD"), cancellable = true)
+    private void fluidlogistics$destroyFluidPackagesWithoutDrops(CallbackInfo ci) {
+        if (!FluidPackageItem.isFluidPackage(inventory.getStackInSlot(0))) {
+            return;
+        }
+
+        inventory.clear();
+        ci.cancel();
+    }
+}
